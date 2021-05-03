dad3dce594f0b72a10ea9148e6c5e5cf0e959c15
Aniket <anirajkul01@gmail.com>
2021-05-03 18:49:18 +0530

    call back url changed

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.render(
     <Auth0Provider domain="aniketkulkarni.us.auth0.com"
         clientId="9yYWQaFFmOhGbBy9CDJ7fhKLCoIpIfoS"
-        redirectUri="http://localhost:3000/all-notes">
+        redirectUri="https://evernoteclone.vercel.app/all-notes">
         <App />
     </Auth0Provider>
     , document.getElementById("root")
