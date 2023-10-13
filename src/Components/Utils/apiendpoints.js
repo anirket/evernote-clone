5b523bf34a6900bd781cfe32f512ea95883d7e13
Aniket <aniket.kulkarni@smallcase.com>
2023-10-14 00:17:02 +0530

    update repo

--- src/Components/Utils/apiendpoints.js
+++ src/Components/Utils/apiendpoints.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://ebvernote-clone-server.herokuapp.com';
+export const BASE_URL = 'https://evernote-server-gnjd.onrender.com';
 export const CREATE_NOTE = '/api/note'; 
 export const UPDATE_NOTE = '/api/note' //-- noteid required
 export const GET_ALL_NOTES = '/api/notes/all' // -- oauthid required
